52d129e7e019004ee857e28efd3f71d3b2553802
1813932878@qq.com <1813932878@qq.com>
2019-12-14 14:15:30 +0800

    0场景准备好给东仔做字幕

---
+++ Assets/Scripts/0 Scene 小物件/SmallObjToScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class SmallObjToScene : CanMutuObj
+{
+    
+
+    private void OnMouseDown()
+    {
+        BeginLoadGameScene();
+        UnityEngine.SceneManagement.SceneManager.LoadScene(((int)m_type).ToString());
+    }
+
+
+    public void BeginLoadGameScene()
+    {
+
+    }
+}
+
+
+
+
---
+++ Assets/Scripts/0 Scene 小物件/Wishlist.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wishlist : CanMutuObj
+{
+    private void OnMouseDown()
+    {
+        Debug.Log("按到了愿望单");
+    }
+}
